80f220eaa01b457586c42e722250925185cec10a
frostnight <frostnightsong@gmail.com>
2021-05-18 17:37:40 +0900

    fix: 마지막 케이스 9 635 무한대 결과 값으로 인해 미리 지승을 구할 수 없음 서로 별개로 해야함

--- src/test/java/bronze/DistributedProcessingTest.java
+++ src/test/java/bronze/DistributedProcessingTest.java
@@ -8,13 +8,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+/**
+ *
+ */
 class DistributedProcessingTest {
 
-	@ParameterizedTest
-	@CsvSource({"1,6,1", "3,7,2187", "6,2,36"})
-	@DisplayName("A의 B제곱 구하기 A^B")
-	void getSquare(double A, double B, double expected){
-		assertThat(Math.pow(A, B)).isEqualTo(expected);
+	private final int COMPUTER_COUNT = 10;
+	int data = 1;
+
+	@Test
+	@DisplayName("스레드 초기화 테스트")
+	void threadTest(double A, double B, double expected){
+		Thread[] t = new Thread[COMPUTER_COUNT];
+		for(int i=0; i < COMPUTER_COUNT; i++) {
+			t[i] = new
+		}
 	}
 
 	void getRemainder(double DataVolume, double computerNum){
